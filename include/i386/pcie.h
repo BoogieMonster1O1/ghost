e4eb937fcba926cfbe9361cbc32af7b2aa2e37d9
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-10-30 00:15:29 +0530

    Constants for pcie

--- include/i386/pcie.h
+++ include/i386/pcie.h
@@ -3,6 +3,12 @@
 
 #include <stdint.h>
 
+#define PCIE_FUNCTION_BRIDGE 0x0
+#define PCIE_FUNCTION_USB_CONTROLLER 0x1
+#define PCIE_FUNCTION_SATA_CONTROLLER 0x3
+
+#define PCIE_VENDOR_ID_INTEL 0x8086
+
 typedef struct {
     uint16_t vendor_id;
     uint16_t device_id;
